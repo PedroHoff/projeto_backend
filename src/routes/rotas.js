4a66e8a2b8d0ac1d45ef7d064dffcd03b2ff8fbf
PedroHoff <pedrohoff87@gmail.com>
2023-11-09 10:05:40 -0300

    rotas do projeto

--- src/routes/rotas.js
+++ src/routes/rotas.js
@@ -1,11 +1,16 @@
 const { Router } = require('express');
 const router = Router();
-response.send('Método GET: listar informações');
-response.send('Método POST: salvar informações');
-router.put('/user/:id', (request, response) => {
+router.get('/listar', (request, response) => {
+    response.send('Método GET: listar informações');
 });
+router.post('/cadastrar', (request, response) => {
+    response.send('Método POST: salvar informações');
+});
+router.put('/user/:id', (request, response) => {
     response.send('Método PUT: atualizar informações');
-    router.delete('/user/:id', (request, response) => {
-    });
-        response.send('Método DELETE: remover informações');
-        module.exports = router;
+});
+router.delete('/user/:id', (request, response) => {
+    response.send('Método DELETE: remover informações');
+});
+
+module.exports = router;
